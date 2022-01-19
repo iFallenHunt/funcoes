3d87610d553eae493b049343259638f6d9ea8a73
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-01-19 13:43:38 -0300

    Adicionar arquivos de projeto.

---
+++ funcoes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três números: ");
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(x, y, z);
+
+            Console.WriteLine("Maior = " + resultado);
+        }
+
+        static int Maior (int a, int b, int c)
+        {
+            int m;
+            if (a >b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+            return m;
+        }
+
+    }
+
+}
